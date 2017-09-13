5e91eb8d916de15d6344427925d5e460b012732a
psfinaki <petersemkin@gmail.com>
2017-09-13 13:15:14 +0200

    Saving data to the database (CW-44)

---
+++ src/ChordinaryWorld.Core/AzureConnector.fs
@@ -0,0 +1,35 @@
+﻿module AzureConnector
+
+open System
+open Microsoft.Azure.Documents.Client
+open Microsoft.Azure.Documents
+
+type DocumentClient with 
+    member this.GetCollectionsLink database =
+        this
+            .CreateDatabaseIfNotExistsAsync(database)
+            .Result
+            .Resource
+            .CollectionsLink
+
+    member this.GetDocumentsLink (link: string) collection =
+        this
+            .CreateDocumentCollectionIfNotExistsAsync(link, collection)
+            .Result
+            .Resource
+            .DocumentsLink
+
+let SaveDocument document = 
+    let uri = "https://chordinaryworld.documents.azure.com:443/"
+    let key = "secret"
+    use client = new DocumentClient(Uri uri, key) 
+    
+    let database = Database()
+    database.Id <- "Music"
+    let collectionsLink = client.GetCollectionsLink database
+
+    let collection = DocumentCollection()
+    collection.Id <- "Songs"
+    let documentsLink = client.GetDocumentsLink collectionsLink collection
+
+    client.CreateDocumentAsync(documentsLink, document).Result |> ignore
---
+++ src/ChordinaryWorld.Core/DatabaseConnector.fs
@@ -0,0 +1,14 @@
+﻿module DatabaseConnector
+
+type Song = { Artist : string; Title : string; Harmonies: int }
+
+let SaveSong (artist, title) harmonies =
+    { Artist = artist; Title = title; Harmonies = harmonies }
+    |> AzureConnector.SaveDocument 
+
+let Save song result = 
+    match result with 
+    | Success number -> SaveSong song number
+    | Failure _ -> ()
+
+    result
