79a4641de9dd337fa0bd49648e3fc82d016acc80
psfinaki <petersemkin@gmail.com>
2017-10-06 16:04:20 +0200

    Prepared console client for multiple features (#58)

---
+++ src/ChordinaryWorld.Console/Features.fs
@@ -0,0 +1,38 @@
+﻿module internal Features
+
+open System
+
+let GetInput() =
+    printfn "Enter the artist"
+    let artist = Console.ReadLine()
+    printfn "Enter the title"
+    let title = Console.ReadLine()
+    (artist, title)
+
+let TranslateWarning = function 
+    | UnknownDatabaseIssue -> 
+        "Something bad happened in database"
+
+let TranslateError = function
+    | ChordsNotFound -> 
+        "Chords are not found"
+    | EmptyInput -> 
+        "Empty input is not allowed"
+    | UnknownFlavours x -> 
+        "Unknown flavours in the tab: " + (Seq.toList x).ToString()
+
+let GetNumberOfHarmonies() =
+    let result = Core.GetNumberOfHarmonies <| GetInput()
+
+    match result with
+    | Success (x, warnings) -> 
+        printfn "Number of harmonies here is %A" x
+
+        warnings 
+        |> Seq.map TranslateWarning 
+        |> Seq.iter Console.WriteLine
+
+    | Failure error -> 
+        error 
+        |> TranslateError 
+        |> Console.WriteLine
--- src/ChordinaryWorld.Console/Program.fs
+++ src/ChordinaryWorld.Console/Program.fs
@@ -1,37 +1,34 @@
 ﻿open System
 
-let ReadData() =
-    printfn "Enter the artist"
-    let artist = Console.ReadLine()
-    printfn "Enter the title"
-    let title = Console.ReadLine()
-    (artist, title)
+let GetUserInput() = 
+    printfn "Select the feature:"
+    printfn "1 - Get number of harmonies"
+
+    let choice = Console.ReadLine()
+    printfn ""
+
+    choice
+
+let TranslateUserInput = function
+    | "1" -> Some Features.GetNumberOfHarmonies
+    |  _  -> None
 
 [<EntryPoint>]
 let main argv = 
     printfn "Welcome to the Chordinary World!"
     printfn ""
+
+    let inputGenerator _ = GetUserInput() 
+
+    let feature =     
+        inputGenerator
+        |> Seq.initInfinite
+        |> Seq.map TranslateUserInput
+        |> Seq.find (fun x -> x.IsSome)
+        |> Option.get
+
+    while true do 
+        feature()
+        printfn ""
     
-    while true do
-        let result = 
-            ReadData()
-            |> Core.GetNumberOfHarmonies 
-
-        match result with
-        | Success (x, warnings) -> 
-            let prettifyWarning = function 
-                | UnknownDatabaseIssue -> "Something bad happened in database"
-            
-            printfn "Number of harmonies here is %A" x
-            warnings |> Seq.map prettifyWarning |> Seq.iter Console.WriteLine
-        | Failure error -> 
-            match error with
-            | ChordsNotFound -> 
-                printfn "Chords are not found"
-            | EmptyInput ->
-                printfn "Empty input is not allowed"
-            | UnknownFlavours x ->
-                printfn "Unknown flavours in the tab: %A" (Seq.toList x)
-
-        Console.WriteLine()
     0
