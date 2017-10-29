e574151a61020d219748c2b6754c8f7931c0ecad
psfinaki <petersemkin@gmail.com>
2017-10-29 13:46:53 +0100

    Added GetArtistTop to Web API (#81)

---
+++ src/ChordinaryWorld.WebService/ArtistTopController.fs
@@ -0,0 +1,28 @@
+﻿module ArtistTopController
+
+open System.Web.Http
+
+type ArtistTopController() =
+    inherit ApiController()
+
+    member x.Get artist =
+        let Unnullify s = if s = null then "" else s 
+
+        let result = artist |> Unnullify |> Core.GetArtistTop
+
+        match result with
+        | Success (top,_) ->
+            x.Ok top
+            :> IHttpActionResult
+        | Failure error ->
+            match error with
+            | BadArtist ->
+                x.BadRequest()
+                :> IHttpActionResult
+            | NoTabsFound ->
+                x.NotFound()
+                :> IHttpActionResult
+            | TabsUnuavailable
+            | BadTabs ->
+                x.InternalServerError()
+                :> IHttpActionResult
