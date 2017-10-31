a04b38c66215a932bc5f7f516a15be6a826885e1
psfinaki <petersemkin@gmail.com>
2017-10-31 12:01:22 +0100

    Minor: refactoring in the Logger

--- src/ChordinaryWorld.Core/Logger.fs
+++ src/ChordinaryWorld.Core/Logger.fs
@@ -4,82 +4,66 @@ open System
 open System.Diagnostics
 
 let LogError = Trace.TraceError
+let DoNotLog = ()
 
-let LogHarmonies song result =
-    let log (artist, title) message =
+let LogHarmonies (artist, title) result =
+    let Log message =
         String.Format("{0} - {1} - {2}", artist, title, message) 
         |> LogError 
 
     match result with
     | Success (_, messages) -> 
         let handle = function
-            | UnknownDatabaseIssue ->
-                "Something bad happened in database"
-                |> log song
+        | UnknownDatabaseIssue ->
+            Log "Something bad happened in database"
 
         Seq.iter handle messages
-    | Failure error ->
-        let handle = function
-            | ChordsNotFound ->
-                "Chords not found"
-                |> log song
-            | EmptyInput ->
-                ()
-            | UnknownFlavours flavours -> 
-                "Unknown flavours: " + (Seq.toList flavours).ToString()
-                |> log song
-            | UnknownDatabaseErrorHarmonies ->
-                "Something very bad happened in database"
-                |> log song
-            | ChordsNotAvailable ->
-                "Problems with chords provider"
-                |> log song
 
-        handle error
+    | Failure error ->
+        match error with
+        | ChordsNotFound ->
+            Log "Chords not found"
+        | EmptyInput ->
+            DoNotLog
+        | UnknownFlavours flavours -> 
+            Log ("Unknown flavours: " + (Seq.toList flavours).ToString())
+        | UnknownDatabaseErrorHarmonies ->
+            Log "Something very bad happened in database"
+        | ChordsNotAvailable ->
+            Log "Problems with chords provider"
 
 let LogCount count result =
-    let log count message =
+    let Log message =
         String.Format("{0} - {1}", count, message) 
         |> LogError 
 
     match result with
     | Success (_,_) -> 
-        ()
+        DoNotLog
     | Failure error ->
-        let handle = function
-            | NegativeCount -> 
-                "Negative count"
-                |> log count
-            | TooBigCount -> 
-                "Too big count"
-                |> log count
-            | UnknownDatabaseErrorTop -> 
-                "Something bad happened in the database"
-                |> log count
-
-        handle error
+        match error with
+        | NegativeCount -> 
+            Log "Negative count"
+        | TooBigCount -> 
+            Log "Too big count"
+        | UnknownDatabaseErrorTop -> 
+            Log "Something bad happened in the database"
 
 let LogArtist artist result =
-    let log artist message =
+    let Log message =
         String.Format("{0} - {1}", artist, message) 
         |> LogError 
 
     match result with
     | Success (_,_) -> 
-        ()
+        DoNotLog
     | Failure error ->
-        let handle = function
-            | BadArtist ->
-                "Invalid artist"
-                |> log artist
-            | NoTabsFound ->
-                "No chords found"
-                |> log artist
-            | TabsUnavailable ->
-                "Cannot reach chord provider"
-                |> log artist
-            | BadTabs ->
-                "All found tabs have problems"
-                |> log artist
-
-        handle error
+        match error with
+        | BadArtist ->
+            Log "Invalid artist"
+        | NoTabsFound ->
+            Log "No chords found"
+        | TabsUnavailable ->
+            Log "Cannot reach chord provider"
+        | BadTabs ->
+            Log "All found tabs have problems"
